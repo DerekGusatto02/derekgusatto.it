59b1eea062e8610f87d2c397119f3d524985cda9
Derek Gusatto <gusattoderek@gmail.com>
2024-08-30 15:31:37 +0200

    Reneme Works -> Projects

---
+++ projects.php
@@ -0,0 +1,5 @@
+<?php
+
+    $paginaHTML=file_get_contents("projects.html");
+
+    echo $paginaHTML;
--- works.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    $paginaHTML=file_get_contents("works.html");
-
-    echo $paginaHTML;
